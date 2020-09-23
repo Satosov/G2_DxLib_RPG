2587b6bc15c81b159c843d2ac02a4cdc69bfd4ba
Satosov <hj-g19027@sist.ac.jp>
2020-09-23 10:00:27 +0900

    map1.tmx,map1.tsx,map2.tsxを追加

---
+++ IMAGE/MAP/map2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="map2" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="../../../../Users/G19027/Downloads/地下水路/VXAce改変/地下迷宮Tile_A1.png" width="512" height="384"/>
+</tileset>
